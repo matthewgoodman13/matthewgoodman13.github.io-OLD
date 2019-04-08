8fa1fbc9887dc2479c4a0fc0075ec3c86fe2ce5e
Matthew Goodman <matthewgoodman13@outlook.com>
2019-04-07 20:30:39 -0400

    save

--- FCC/FEL/random-quote-generator-v2/src/index.js
+++ FCC/FEL/random-quote-generator-v2/src/index.js
@@ -4,6 +4,31 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { combineReducers, createStore } from 'redux';
+
+function reducer(state, action) {
+    if (action.type === 'changeState') {
+        return action.payload.newState;
+    }
+    return 'State';
+}
+
+
+const store = createStore(reducer);
+
+console.log(store.getState());
+
+const action = {
+    type: 'changeState',
+    payload: {
+        newState: 'New State'
+    }
+};
+
+store.dispatch(action);
+
+console.log(store.getState());
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
